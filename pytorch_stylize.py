37adc513f071eaa25a344100e6d149d316fa7d76
Kyungsu Kim <kks1502@postech.ac.kr>
2017-11-13 01:40:42 +0900

    pytorch_stylize model is now working.

--- pytorch_stylize.py
+++ pytorch_stylize.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+import os
+
 import pytorch_model
 import torch
 from torch.autograd import Variable
@@ -11,39 +14,49 @@ import pdb
 
 def main():
 
-  model_path = '/home/kyungsukim/AMT/model/AMT_pytorch_model_ReLU_352'
-  content_path = '/home/kyungsukim/AMT/wav/guitar_content.wav'
-  style_path = '/home/kyungsukim/AMT/wav/guitar_style.wav'
-  save_path = '/home/kyungsukim/AMT/result/guitar_combined_cqt.npy'
-  content_coeffs = [0.0001,0.0001]
-  style_coeffs = [0.0000001,0.0000001]
-  num_iteration = 10
-  learning_rate = 0.00000001
+  model_file = '/home/kyungsukim/AMT/model/AMT_pytorch_model_ReLU_352'
+  content_file = '/home/kyungsukim/AMT/wav/guitar_content.wav'
+  style_file = '/home/kyungsukim/AMT/wav/guitar_style.wav'
+  save_path = '/home/kyungsukim/AMT/result/cont=1.,1._style=1e-8,1e-8_optim=SGD_lr=0.1=_mom=0.9_/'
+  content_coeffs = [1.,1.]
+  style_coeffs = [1e-8,1e-8]
+  num_iteration = 100000
+  save_freq = 1000
+  learning_rate = 0.1
 
   net = pytorch_model.AMT().cuda()
-  net.load_state_dict(torch.load(model_path))
+  net.load_state_dict(torch.load(model_file))
+  for param in net.parameters():
+      param.requires_grad = False
   print('model restored.')
   dtype = torch.cuda.FloatTensor
 
-  content_wav= utils.load_wav(content_path)[:180000]
-  style_wav = utils.load_wav(style_path)
+  content_wav= utils.load_wav(content_file)[:180000]
+  style_wav = utils.load_wav(style_file)
   content_cqt = utils.standardize(utils.cqt(content_wav),axis=1)
   style_cqt = utils.standardize(utils.cqt(style_wav),axis=1)
-  #pdb.set_trace()
   content_cqt = content_cqt.reshape([1,1,-1,264])
   style_cqt = style_cqt.reshape([1,1,-1,264])
 
-  content_var = Variable(torch.Tensor(content_cqt).type(dtype))
-  style_var= Variable(torch.Tensor(style_cqt).type(dtype))
+  content_fname = 'content.npy'
+  style_fname = 'style.npy'
+  content_full_fname = os.path.join(save_path,content_fname)
+  style_full_fname = os.path.join(save_path,style_fname)
+  np.save(content_full_fname,content_cqt)
+  np.save(style_full_fname,style_cqt)
+  print('Content, Style CQT is saved.')
 
-  stylized = stylize(net,content_var,style_var,content_coeffs,
-                     style_coeffs,num_iteration,learning_rate)
-  import pdb; pdb.set_trace()
+
+  content_var = Variable(torch.Tensor(content_cqt).type(dtype),requires_grad=False)
+  style_var= Variable(torch.Tensor(style_cqt).type(dtype),requires_grad=False)
+
+  stylized = stylize(net,content_var,style_var,content_coeffs,style_coeffs,
+                     learning_rate,num_iteration,save_freq,save_path)
   np.save(save_path,stylized)
 
 
-def stylize(net, content, style, content_coeffs, style_coeffs, num_iteration,
-            learning_rate):
+def stylize(net, content, style, content_coeffs, style_coeffs, learning_rate,
+            num_iteration,save_freq,save_path):
 
   content_size = content.size()
   #combined = Variable(torch.from_numpy(np.random.normal(size = content_size,
@@ -54,31 +67,42 @@ def stylize(net, content, style, content_coeffs, style_coeffs, num_iteration,
   feats_content = net.features(content)
   grams_style = net.grams(style)
 
-  loss = 0.
-  loss_func = F.mse_loss
-  optimizer = optim.SGD([combined],lr=learning_rate)
-  import pdb; pdb.set_trace()
+  loss_func = nn.MSELoss()
+  optimizer = optim.SGD([combined],lr=learning_rate,momentum=0.9)
   for i in range(num_iteration):
+    content_loss,style_loss = 0.,0.
     feats_combined = net.features(combined)
     grams_combined = net.grams(combined)
 
+    feat_losses = []
+    gram_losses = []
     for j in range(len(feats_combined)):
-      feat_loss = content_coeffs[j]*loss_func(feats_combined[j],feats_content[j]).backward(retain_graph=True)
-      loss = loss + feat_loss
+      feat_loss = content_coeffs[j]*loss_func(feats_combined[j],feats_content[j])
+      feat_loss.backward(retain_graph=True)
+      content_loss += feat_loss
 
     for j in range(len(grams_combined)):
-      gram_loss = content_coeffs[j]*loss_func(grams_combined[j],grams_style[j])
-      #loss = loss - gram_loss
+      gram_loss = style_coeffs[j]*loss_func(grams_combined[j],grams_style[j])
+      gram_loss.backward(retain_graph=True)
+      style_loss += gram_loss
 
+    print("{}th iteration\nContent Loss : {}\nStyle Loss : {}".format(i,
+          content_loss.data.cpu().numpy(),style_loss.data.cpu().numpy()))
+    optimizer.step()
     optimizer.zero_grad()
-    loss.backward(retain_graph=True)
-    loss = optimizer.step()
-    print("{}th iteration\nLoss : {}".format(i,loss.data.cpu().numpy()))
+
+    if(i % save_freq == save_freq-1):
+      stylized = combined.data.cpu().numpy()
+      save_file_name = '{}th_combined-content:{:.3f}_style:{:.3f}.npy'.format(i+1,
+        content_loss.data.cpu().numpy()[0],style_loss.data.cpu().numpy()[0])
+      save_full_path = os.path.join(save_path,save_file_name)
+      np.save(save_full_path,stylized)
+      print('{}th model saved'.format(i+1))
+
 
   return combined.data.cpu().numpy()
 
-def mse_loss(input,target):
-    return torch.sum((input - target)*(input - target)) / input.data.nelement()
+
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,13 +1,38 @@
-import pdb
+from __future__ import print_function
+
 import numpy as np
-import utils
 
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
 
-a = np.ones([100,264])
+iteration = 1000
+lr = 0.001
+x = Variable(torch.randn(5,2,2),requires_grad=True)
+target = Variable(torch.randn(1,5,5))
+optimizer = torch.optim.SGD([x],lr=lr)
+loss = torch.nn.MSELoss()
+#import pdb; pdb.set_trace()
+for i in range(iteration):
+  x_view = x.view(5,-1)
+  gram = torch.mm(x_view,x_view.t())
+  gram = gram / 4.
+  gram = gram.view(1,5,5)
+  gram = F.relu(F.max_pool2d(gram,(1,1)))
+  output = loss(gram,target)
+  output.backward(retain_graph=True)
+  optimizer.step()
+  optimizer.zero_grad()
+  #x.data -= lr*x.grad.data
+  #x.grad.data.zero_()
+  print('loss : {}'.format(output))
+'''
 
-pdb.set_trace()
-
-b = utils.window(a,7)
-
-
-print b 
+loss = torch.nn.MSELoss()
+input = Variable(torch.randn(3,3),requires_grad=True)
+m  = input + 3
+target = Variable(torch.randn(3,3))
+output = loss(m,target)
+output.backward()
+import pdb; pdb.set_trace()
+'''
